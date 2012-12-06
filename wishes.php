84a3d7302979e2eb7317d44d356d9542db0fd9f4
jjj <jjonj@hotmail.com>
2012-12-06 22:33:30 +0100

    æøå rettelser, nu escapet i main content

--- wishes.php
+++ wishes.php
@@ -3,21 +3,23 @@
 ?>
 	<div id="page">
 		<div id="content">
-			<h2>Jacob's Julegave ønsker!</h2>
+			<h2>Jacob's Julegave &#248;nsker!</h2>
 			<p><img src="../images/xmastop.jpg" width=750 alt="" /></p>
+			
 			<p>
+			
 			<h3 class="wishes">
 			<table>
-			<tr><td>- Rygsæk (Til bærbar og bøger)</tr>
+			<tr><td>- Rygs&#230;k (Til b&#230;rbar og b&#248;ger)</tr>
 			<tr><td>- Opslags bord (Noget i stil med den <a href="http://www.harald-nyborg.dk/images/1063.jpg">HER</a>)</tr>
-			<tr><td>- Håndstøvsuger</tr>
-			<tr><td>- Æggekoger</td></tr>
-			<tr><td>- Gæste madras</tr>
+			<tr><td>- H&#229;ndst&#248;vsuger</tr>
+			<tr><td>- &#198;ggekoger</td></tr>
+			<tr><td>- G&#230;ste madras</tr>
 			<tr><td>- Boremaskine</tr>
 			<tr><td>- Futsko</tr>
-			<tr><td>- Stor vasketøjskurv</tr>
+			<tr><td>- Stor vasket&#248;jskurv</tr>
 			<tr><td>- Brug cykel</tr>
-			<tr><td>- Halstørklæde</tr>
+			<tr><td>- Halst&#248;rkl&#230;de</tr>
 			</table>
 			</h3>
 			</p>
